010872c8c6188d96690a95373cf93a5841834923
KangSungHyun <ksh901016@naver.com>
2015-10-30 15:12:34 +0900

    web.xml 스프링 설정 ~.xml 들 설명 주석

---
+++ src/main/java/com/cdg/bookcafe/domain/Board.java
@@ -0,0 +1,61 @@
+package com.cdg.bookcafe.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+//럼복 설치하기 lombok
+public class Board {
+	private int boardNo;
+	private String title;
+	private String contents;
+	private int userNo;
+	private Date createDate;
+	private Date updateDate;
+	private int hit;
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	
+	
+}
---
+++ src/main/java/com/cdg/bookcafe/mapper/BoardMapper.java
@@ -0,0 +1,8 @@
+package com.cdg.bookcafe.mapper;
+
+import com.cdg.bookcafe.domain.Board;
+
+public interface BoardMapper {
+
+	public void insertBoarid(Board board);
+}
